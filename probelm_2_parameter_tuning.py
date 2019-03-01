3ebef30a444959720277866ce629d37a60afb795
Sanchit Goel <sanchitgoel230296@gmail.com>
2019-03-01 16:08:40 -0500

    problem 2 results

--- probelm_2_parameter_tuning.py
+++ probelm_2_parameter_tuning.py
@@ -1,6 +1,12 @@
+import time
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from glob import glob
+
+from matplotlib import transforms
 
 from xgboost import XGBClassifier
 from sklearn.ensemble import BaggingClassifier
@@ -12,6 +18,9 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -20,19 +29,33 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
 
 # Importing the main data
-df = pd.read_csv('kplr_dr25_inj1_tces.csv', header = 0)
 
+filenames = glob('kplr_dr25_inj*.csv')
+df = pd.concat([pd.read_csv(f) for f in filenames], ignore_index = True)
+'''
+df = pd.read_csv('kplr_dr25_inj1_tces.csv', header = 0)
+'''
 print('Size: ')
 print(df.shape)
 
+
+temp_df = df.iloc[:, 0:15]
+df_drop = temp_df[temp_df.isnull().any(axis=1)]
+temp_df = temp_df.drop(df_drop.index.values)
+print(temp_df.shape)
+
+X = temp_df.iloc[:, 1:14]
+Y = temp_df.iloc[:, 14]
+cols_indices = X.columns
+'''
 df['Disp'] = df['Disp'].replace('PC', 1)
 df['Disp'] = df['Disp'].replace('FP', 0)
 
 X = df.iloc[:,10:25]
 Y = df.iloc[:,2]
-
+'''
 # Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 10, stop = 350, num = 5)]
+n_estimators = [int(x) for x in np.linspace(start = 10, stop = 350, num = 10)]
 
 # Maximum number of levels in tree
 max_depth = [int(x) for x in np.linspace(start = 2, stop = 30, num = 10)]
@@ -44,10 +67,10 @@ min_samples_split = [2, 5, 10]
 min_samples_leaf = [1, 2, 4]
 
 # Minimum number of neighbors required for each node
-n_neighbors = [int(x) for x in np.linspace(start = 10, stop = 3, num = 30)]
+n_neighbors = [int(x) for x in np.linspace(start = 3, stop = 30, num = 10)]
 
 # Minimum Power parameter required for the Minkowski metric
-p = [int(x) for x in np.linspace(start = 10, stop = 1, num = 5)]
+p = [int(x) for x in np.linspace(start = 1, stop = 5, num = 10)]
 
 knc = KNeighborsClassifier() #n_neighbors, p
 abc = AdaBoostClassifier() #n_estimators
@@ -64,7 +87,7 @@ plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.jet(np.linspace(0, 1, len(ma
 ###############################################################################################################
 #Create the param frid
 param_grid = dict(n_neighbors = n_neighbors,
-				  p = p)
+                  p = p)
 
 knc_grid_search = GridSearchCV(knc, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
 knc_grid_result = knc_grid_search.fit(X, Y)
@@ -76,7 +99,7 @@ stds = knc_grid_result.cv_results_['std_test_score']
 params = knc_grid_result.cv_results_['params']
 print('KNeighborsClassifier')
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 ###############################################################################################################
 #Create the param frid
@@ -92,13 +115,13 @@ stds = abc_grid_result.cv_results_['std_test_score']
 params = abc_grid_result.cv_results_['params']
 print('AdaBoostClassifier')
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 
 ###############################################################################################################
 #Create the param frid
 param_grid = dict(n_estimators = n_estimators,
-				  max_depth = max_depth)
+                  max_depth = max_depth)
 
 xgb_grid_search = GridSearchCV(xgb, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
 xgb_grid_result = xgb_grid_search.fit(X, Y)
@@ -109,13 +132,13 @@ means = xgb_grid_result.cv_results_['mean_test_score']
 stds = xgb_grid_result.cv_results_['std_test_score']
 params = xgb_grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 ###############################################################################################################
 #Create the param frid
 param_grid = dict(max_depth = max_depth,
-				  min_samples_split = min_samples_split,
-				  min_samples_leaf = min_samples_leaf)
+                  min_samples_split = min_samples_split,
+                  min_samples_leaf = min_samples_leaf)
 
 dtc_grid_search = GridSearchCV(dtc, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
 dtc_grid_result = dtc_grid_search.fit(X, Y)
@@ -126,14 +149,14 @@ means = dtc_grid_result.cv_results_['mean_test_score']
 stds = dtc_grid_result.cv_results_['std_test_score']
 params = dtc_grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 ###############################################################################################################
 #Create the param frid
 param_grid = dict(n_estimators = n_estimators,
-				  max_depth = max_depth,
-				  min_samples_split = min_samples_split,
-				  min_samples_leaf = min_samples_leaf)
+                  max_depth = max_depth,
+                  min_samples_split = min_samples_split,
+                  min_samples_leaf = min_samples_leaf)
 
 etc_grid_search = GridSearchCV(etc, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
 etc_grid_result = etc_grid_search.fit(X, Y)
@@ -144,7 +167,7 @@ means = etc_grid_result.cv_results_['mean_test_score']
 stds = etc_grid_result.cv_results_['std_test_score']
 params = etc_grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 
 rfc_grid_search = GridSearchCV(rfc, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
@@ -156,7 +179,7 @@ means = rfc_grid_result.cv_results_['mean_test_score']
 stds = rfc_grid_result.cv_results_['std_test_score']
 params = rfc_grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 
 gbc_grid_search = GridSearchCV(gbc, param_grid, scoring = "neg_log_loss", n_jobs = -1, cv = kfold)
@@ -168,4 +191,4 @@ means = gbc_grid_result.cv_results_['mean_test_score']
 stds = gbc_grid_result.cv_results_['std_test_score']
 params = gbc_grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-	print("%f (%f) with: %r" % (mean, stdev, param))
+    print("%f (%f) with: %r" % (mean, stdev, param))
